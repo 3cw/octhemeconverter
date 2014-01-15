288d67d1d5697bc3f51f1c9d269d53fe54fc53dc
mrsmall <a.klein@threecogwheels.com>
2014-01-15 17:40:22 +0100

    Directory structure convertion added

--- src/main/java/com/threecw/ecommerce/LanguageConverter.java
+++ src/main/java/com/threecw/ecommerce/LanguageConverter.java
@@ -1,6 +1,12 @@
 package com.threecw.ecommerce;
 
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -38,9 +44,9 @@ public class LanguageConverter {
 				+ "}\n"
 				+ "");
 		
-		String code=phpCode.toString();
+		String code=phpCode.toString().replaceFirst("<\\?php", "").replaceFirst("\\?>", "");
 
-		QuercusProgram programm=quercus.parseCode(quercus.createString(code));
+		QuercusProgram programm=quercus.parseCode(code);
 		ByteArrayOutputStream outStream=new ByteArrayOutputStream();
 		VfsStream writer=new VfsStream(null, outStream);
 
@@ -58,4 +64,38 @@ public class LanguageConverter {
 		return result;
 	}
 	
+	private void convertPhpFileToPropertiesFile(File phpFile, File file) throws IOException {
+		FileInputStream is=new FileInputStream(phpFile);
+		String output=convertToString(is);
+		
+		BufferedWriter out=new BufferedWriter(new FileWriter(file));
+		out.write(output);
+		out.close();
+	}
+
+
+	public void convertDir(File themeLanguageDir, String sourceLanguage, String targetLocale) throws IOException{
+		File localeDir=new File(themeLanguageDir, targetLocale);
+		if (!localeDir.exists())
+			localeDir.mkdirs();
+		File languageDir=new File(themeLanguageDir, sourceLanguage);
+		
+		for (File subDir : languageDir.listFiles()){
+			File[] files = subDir.listFiles();
+			File localeSubDir=new File(localeDir, subDir.getName());
+			if (!localeSubDir.exists())
+				localeSubDir.mkdirs();
+
+			if (files!=null){
+				for (File phpFile : files){
+					String propertiesFileName=phpFile.getName();
+					propertiesFileName=propertiesFileName.replaceAll(".php", ".properties");
+					convertPhpFileToPropertiesFile(phpFile, new File(localeSubDir, propertiesFileName));
+				}
+			}
+		}
+		File mainPhpFile=new File(languageDir, sourceLanguage+".php");
+		convertPhpFileToPropertiesFile(mainPhpFile, new File(localeDir, "main.properties"));
+	}
+
 }
